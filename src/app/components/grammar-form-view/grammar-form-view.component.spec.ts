3036e6c1531617b842b09de9cebba583723bb97f
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-11-13 11:50:48 -0500

    Adding template spec for form

--- src/app/components/grammar-form-view/grammar-form-view.component.spec.ts
+++ src/app/components/grammar-form-view/grammar-form-view.component.spec.ts
@@ -1,12 +1,13 @@
 import {Component, DebugElement} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormGroupDirective, ReactiveFormsModule} from '@angular/forms';
+import {FormGroup, FormGroupDirective, NgControl, ReactiveFormsModule} from '@angular/forms';
 import {By} from '@angular/platform-browser';
 import {Subject} from 'rxjs/Subject';
 
 import {Grammar} from '../../models/grammar';
 import {Environment} from '../../services/environment.service';
 import {GrammarFormViewComponent} from './grammar-form-view.component';
+import {GrammarRule} from '../../models/grammar-rule';
 
 type TestNonTerminals = 'S' | 'A';
 type TestTerminals = 'a' | 'b';
@@ -93,27 +94,69 @@ describe('GrammarFormViewComponent', () => {
             expect(formArray.control).toBe(component.form);
         });
 
-        it('should have inputs for the grammar and the new rule', () => {
-            const inputs = fixture.debugElement.queryAll(By.css('input'));
+        describe('inputs', () => {
+            let inputs: DebugElement[];
+            let rules: GrammarRule<string, string>[];
 
-            const rules = Object.values(hostComponent.grammar.rules);
+            beforeEach(() => {
+                inputs = fixture.debugElement.queryAll(By.css('input'));
+                rules = hostComponent.grammar.rules;
+            });
 
-            expect(inputs.length).toBe((rules.length + 1) * 2);
+            it('should be enough for the grammar and the new rule', () => {
+                expect(inputs.length).toBe((rules.length + 1) * 2);
+            });
+
+            it('should connect to form', () => {
+                for (let i = 0; i < inputs.length; ++i) {
+                    const row = component.form.controls[Math.floor(i / 2)] as FormGroup;
+                    const control = inputs[i].injector.get(NgControl, null);
+                    expect(control).toBeTruthy();
+
+                    if (i % 2 === 0) {
+                        expect(control.control).toBe(row.controls.nonTerminal);
+                    } else {
+                        expect(control.control).toBe(row.controls.production);
+                    }
+                }
+            });
+
+            describe('last', () => {
+                it('should call addBlankRule() once when focused', () => {
+                    const last = inputs[inputs.length - 1];
+
+                    spyOn(component, 'addBlankRule');
+
+                    last.triggerEventHandler('focus', null);
+
+                    expect(component.addBlankRule).toHaveBeenCalledTimes(1);
+                });
+            });
         });
 
-        it('should have a button to remove any rule except the start rule and the new rule', () => {
-            const buttons = fixture.debugElement.queryAll(By.css('button'));
-
-            const rules = Object.values(hostComponent.grammar.rules);
-
-            expect(buttons.length).toBe(rules.length - 1);
-
-            spyOn(component, 'onRemoveRule');
-
-            for (const [removeButton, index] of buttons.map((btn, ix) => [btn, ix + 1] as [DebugElement, number])) {
-                removeButton.triggerEventHandler('click', null);
-                expect(component.onRemoveRule).toHaveBeenCalledWith(index);
-            }
+        it('should have text for first input row', () => {
+            const firstCell = fixture.debugElement.query(By.css('td:first-child'));
+            expect(firstCell).toBeTruthy();
+            expect(firstCell.nativeElement.textContent).not.toBe('');
         });
+
+        it(
+            'should have a button calling onRemoveRule(ruleIndex) once on all rules except the start rule and the new rule',
+            () => {
+                const buttons = fixture.debugElement.queryAll(By.css('button'));
+
+                const rules = Object.values(hostComponent.grammar.rules);
+
+                expect(buttons.length).toBe(rules.length - 1);
+
+                const spy = spyOn(component, 'onRemoveRule');
+
+                for (const [removeButton, index] of buttons.map((btn, ix) => [btn, ix + 1] as [DebugElement, number])) {
+                    removeButton.triggerEventHandler('click', null);
+                    expect(spy.calls.argsFor(index - 1)).toEqual([index]);
+                    expect(spy.calls.count()).toEqual(index);
+                }
+            },
+        );
     });
 });
